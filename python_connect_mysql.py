bc3ec71603237f7ffe53dc36d00ca0f4ae9ce087
anarlewang <96459983+anarlewang@users.noreply.github.com>
2022-04-07 02:13:21 -0400

    Added lines to read in text & instagram account

--- python_connect_mysql.py
+++ python_connect_mysql.py
@@ -13,4 +13,11 @@ connection_engine = sqlalchemy.create_engine('mysql+pymysql://' + user + ':' + p
 # You can use user, password, port and db info to connect via MySQL Workbench
 
 # Write your SQL through this way in Python (returns a Pandas dataframe)
+## Show all tables:
+pd.read_sql("show tables", con = connection_engine)
+## Read in LIWC2015 text table:
+text_data = pd.read_sql('select * from ins_text_data',con=connection_engine)
+## Read in account info table
+instagram_data = pd.read_sql('select * from ins_account_data',con=connection_engine)
+
 data = pd.read_sql('select * from ins_account_data',con=connection_engine)
